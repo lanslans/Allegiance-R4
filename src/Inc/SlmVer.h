dad3fbb0e381e34e19a8aed8874e6a20ecbebd18
Nick Pirocanac <nick@zaphop.com>
2017-11-29 22:45:58 -0600

    Retargeting zlib to win10 platform

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -2,8 +2,8 @@
 #define rmj		1
 #define rmm		1
 
-#define rel     1000000.00       // mmf added to designate release number for a given build
+#define rel     1000001.00       // mmf added to designate release number for a given build
 
-#define rup		100410  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
+#define rup		211335  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
